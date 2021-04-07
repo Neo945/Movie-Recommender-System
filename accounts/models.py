b9e05e56ee6f7d7e3d154e4ce21d188bb55c532e
Neo945 <shreeshsrvstv@gmail.com>
2021-04-07 12:20:03 +0530

    Added Rating in user history model and serializer Removed sample recommender system

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +9,4 @@ class Profile(models.Model):
 class History(models.Model):
     user = models.ForeignKey(Profile,on_delete=models.CASCADE)
     movies = models.ForeignKey('movies.Movie',on_delete=models.CASCADE)
+    user_rating = models.IntegerField(null=False,validators=[MaxValueValidator(10),MinValueValidator(1)])
--- movies/recommend.py
+++ movies/recommend.py
@@ -23,40 +23,10 @@ def recommend_CB(list_movies):
         similar += similar_movies
     return similar
     
-def is_similar(qs,user_history):
-    similarity_count = 0
-    list_ = []
-    for q in qs.order_by('id'):
-        if user_history.filter(movies=q.movies).exists():
-            similarity_count+=1
 
-    for uh in user_history.order_by('id'):
-        if not qs.filter(movies=uh.movies).exists():
-            list_.append(uh.movies.id)
-    if similarity_count==0:
-        return (False,None)
-    else:
-        return (True,list_)
-
-def user_recommend(user):
-    qs = History.objects.filter(user=user).order_by('id')
-    all_qs = History.objects.all()
-    profile_qs = Profile.objects.all()
-    for profile in profile_qs:
-        user_history = all_qs.filter(user=profile).order_by('id')
-        (is_sim,next) = is_similar(qs,user_history)
-        if is_sim:
-            print(next)
-            return next
-    return None
 
 def recommend_movies(list_watch,user):
     cb_list = recommend_CB(list_watch)
-    user_list = user_recommend(user=user) or []
-    for data in cb_list:
-        if data[0]+1 in user_list:
-            # Doubt
-            pass
     k = []
     count = 0
     list_ = sorted(cb_list,key=lambda x:x[1],reverse=True)
