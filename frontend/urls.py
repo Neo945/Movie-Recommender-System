40420839031fb5a3ba2e8ae7cd93edf99de17b92
Neo945 <shreeshsrvstv@gmail.com>
2021-04-13 19:40:55 +0530

    All pages to DTL

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,6 @@ from django.views.generic.base import TemplateView
 app_name = 'frontend'
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name="pages/auth.html")),
+    path('',TemplateView.as_view(template_name="pages/dashboard.html")),
     # path('',TemplateView.as_view(template_name="pages/profile.html")),
 ]
